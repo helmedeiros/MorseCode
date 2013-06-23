4cfed0b405486a82c519ac70577a976ecd36db42
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-06-23 12:03:39 -0300

    Increased tests to covert more words together, also changed to have phrases with special characters.

--- src/test/java/br/com/helmed/morsecode/encode/MorseEncodeTest.java
+++ src/test/java/br/com/helmed/morsecode/encode/MorseEncodeTest.java
@@ -273,5 +273,14 @@ public class MorseEncodeTest {
     @Test public void shouldReturnTheTextCorrespondentWithDoubleSpaceBetweenWords() throws Exception {
         assertEquals(". -- .- -. -.-. .. .--. .- - .. --- -.  .--. .-. --- -.-. .-.. .- -- .- - .. --- -.",
                 MorseEncode.encodeWithSpace("Emancipation Proclamation"));
+
+        assertEquals("-... -.--  - .... .  .--. .-. . ... .. -.. . -. -  --- ..-.  ..- -. .. - . -..  ... - .- - . ...  --- ..-.  .- -- . .-. .. -.-. .-",
+                MorseEncode.encodeWithSpace("By the president of united states of america"));
+    }
+
+    @Test public void shouldReturnTheTextCorrespondentWithDoubleSpaceBetweenWordsWithSpecialCharacters() throws Exception {
+        assertEquals(".-- .... . .-. . .- ... --..--  --- -.  - .... .  - .-- . -. - -.-- -...- ... . -.-. --- -. -..  -.. .- -.--  --- ..-.  ... . .--. - . -- -... . .-. --..--  .. -.  - .... .  -.-- . .- .-.  --- ..-.  --- ..- .-.  .-.. --- .-. -..",
+                MorseEncode.encodeWithSpace("Whereas, On the Twenty=Second day of September, in the year of our Lord"));
+
     }
 }
